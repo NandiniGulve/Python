7ef80cb14d8d5b5a723194b83dce56dcd17f358c
NandiniGulve <nandini.gulve005@gmail.com>
2025-02-07 11:09:44 +0530

    Add files via upload

--- third.py
+++ third.py
@@ -3,7 +3,7 @@ import pandas
 import streamlit as st
 
 
-st.title("Welcome Everyon, we are learning pyhton")
+st.title("Welcome Everyon, This is my first Application!")
 st.write("Python requires practice")
 data=pandas.DataFrame({'c1':[10,20,30,40],'c2':['A','B','C','D']})
 st.write("Below is the data Table")
@@ -12,5 +12,5 @@ st.write(data)
 chart_data=pandas.DataFrame(np.random.randn(20,3),columns=['A','B','C'])
 st.line_chart(chart_data)
 st.bar_chart(chart_data)
-st.altair_chart(chart_data)
+
 #st.write(chart_data)
